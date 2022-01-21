db2aff620fcb29e8a53999c62e4c1d6f365a85f5
subhosantra <45712035+subhosantra@users.noreply.github.com>
2022-01-22 01:20:42 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
+var myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
   port: '443'
